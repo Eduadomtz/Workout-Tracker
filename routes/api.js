a2e3631a11684a194c482bf0bfd5301f025f22d0
Eduardo Martinez <eduardo31794@gmail.com>
2021-08-02 12:13:30 -0500

    Adding routes

---
+++ routes/api.js
@@ -0,0 +1,66 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find()
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/:id", (req, res) => {
+    let id = req.params.id;
+    Workout.findById(id)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/workouts/bulk", ({ body }, res) => {
+    Workout.insertMany(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json.apply(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    let id = req.params.id;
+    Workout.findByIdAndUpdate(id, req.body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
